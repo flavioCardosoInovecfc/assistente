2ab640930849762c7e424bd1da08210bbc394d3b
Flavio Cardoso <flaviokardoso@gmail.com>
2023-10-11 07:55:14 -0300

    asdas

--- assistente.bat
+++ assistente.bat
@@ -7,7 +7,7 @@ ECHO "Iniciando seu Assistente Inove"
 If exist "C:\assistente" (
 	
     xcopy /e /h /d /y C:\assistente\dist\assistente\*.db C:\tempassistente\
-	rmdir /S/Q C:\assistente 
+	rmdir /S/Q C:\assistente
 	git clone https://github.com/flavioCardosoInovecfc/assistente.git
 	xcopy /e /h /d /y C:\tempassistente\*.db C:\assistente\dist\assistente\
 	rmdir /S/Q C:\tempassistente 
